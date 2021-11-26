cc643ad5e48280eaa156f53fac735c26fb0db624
Dragan Pepic <nedimco2006@gmail.com>
2021-11-26 18:32:26 +0100

    Add project files.

---
+++ BlazorApp2/Server/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApp2.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string Zklj()
+            => "EVO SA SEEERVERA!";
+    }
+}
